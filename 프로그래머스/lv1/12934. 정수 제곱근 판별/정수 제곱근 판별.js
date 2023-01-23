1648d8b9d7e82999eb77dd6cf3fdea3d9358405d
liberty556 <56855262+cabbage556@users.noreply.github.com>
2023-01-24 00:21:59 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12934. 정수 제곱근 판별/정수 제곱근 판별.js
@@ -0,0 +1,12 @@
+function solution(n) {
+    let answer = -1;
+    
+    for (let i = 1; i <= n; i++) {
+        if (i * i === n) {
+            answer = i;
+            break;
+        }
+    }
+    
+    return answer === -1 ? -1 : (answer + 1) * (answer + 1);
+}
