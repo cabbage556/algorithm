63f43699dfcb6eebdb4e97af623db591a82ec557
liberty556 <56855262+cabbage556@users.noreply.github.com>
2023-01-25 10:41:09 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 0.26 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
+++ 프로그래머스/lv1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
@@ -1,6 +1,21 @@
 function solution(arr) {
-    const min = Math.min(...arr);
-    const minIdx = arr.indexOf(min);
-    arr.splice(minIdx, 1);
-    return arr.length === 0 ? [-1] : arr;
+    // const min = Math.min(...arr);
+    // arr.splice(arr.indexOf(min), 1);
+    // return arr.length === 0 ? [-1] : arr;
+    
+    // 수업 풀이
+    const answer = [];
+    
+    // 1. 최소값 찾기
+    let min = arr[0];
+    arr.forEach(el => {
+        if (el < min) min = el;
+    });
+    
+    // 2. 최소값 제외한 숫자만 answer에 추가
+    arr.forEach(el => {
+        if (el !== min) answer.push(el);
+    });
+    
+    return answer.length === 0 ? [-1] : answer;
 }
