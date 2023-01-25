db473f470f2f7fb9d5d95380a50c41fc471f3452
liberty556 <56855262+cabbage556@users.noreply.github.com>
2023-01-25 10:16:56 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.03 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/12934. 정수 제곱근 판별/정수 제곱근 판별.js
+++ 프로그래머스/lv1/12934. 정수 제곱근 판별/정수 제곱근 판별.js
@@ -1,12 +1,27 @@
 function solution(n) {
+//     let answer = -1;
+    
+//     for (let i = 1; i <= n; i++) {
+//         if (i * i === n) {
+            // answer = (i + 1) * (i + 1);
+//             break;
+//         }
+//     }
+    
+    // return answer === -1 ? answer : answer * answer;
+    
+    // 수업 풀이
     let answer = -1;
     
-    for (let i = 1; i <= n; i++) {
+    // i * i === n, i는 n의 제곱근
+    for (let i = 1; i * i <= n; i++) {
         if (i * i === n) {
-            answer = i;
-            break;
+            // 제곱근을 찾은 경우
+            answer = i + 1;
+            return answer * answer;
         }
     }
     
-    return answer === -1 ? answer : (answer + 1) * (answer + 1);
+    // 제곱근을 찾지 못한 경우
+    return answer;
 }
