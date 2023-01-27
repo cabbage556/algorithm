e221d7dac8b1e1f4a80d8200222c4726b35ff917
liberty556 <56855262+cabbage556@users.noreply.github.com>
2023-01-27 09:53:05 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
+++ 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
@@ -1,10 +1,7 @@
 function solution(numbers) {
     const zeroNine = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-    let answer = 0;
-    
-    zeroNine.forEach(el => {
-        if (!numbers.includes(el)) answer += el;
-    });
-    
+    const answer = zeroNine
+                        .filter(el => numbers.includes(el))
+                        .reduce(((acc, cur) => acc - cur), 45);
     return answer;
 }
