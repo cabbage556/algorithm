192177cd379b65cc77d9d2c679f4ac0bdc6bf216
liberty556 <56855262+cabbage556@users.noreply.github.com>
2023-01-29 13:07:52 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.17 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.js
@@ -0,0 +1,12 @@
+function solution(n, m) {
+    let gcd = 1;
+    let lcm = 1;
+    
+    for (let i = 2; i <= Math.min(n, m); i++) {
+        if (n % i === 0 && m % i === 0) gcd = i;
+    }
+    
+    lcm = n * m / gcd;
+    
+    return [gcd, lcm];
+}
