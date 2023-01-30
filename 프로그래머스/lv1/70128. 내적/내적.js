015f1784d487f8a0b698860ef4e5c977d4490679
liberty556 <56855262+cabbage556@users.noreply.github.com>
2023-01-30 10:14:19 +0900

    [level 1] Title: 내적, Time: 0.08 ms, Memory: 33.2 MB -BaekjoonHub

--- 프로그래머스/lv1/70128. 내적/내적.js
+++ 프로그래머스/lv1/70128. 내적/내적.js
@@ -1,7 +1,12 @@
 function solution(a, b) {
-    let answer = 0;
-    a.forEach((el, idx) => {
-        answer += el * b[idx];
-    });
+    // let answer = 0;
+    // for (let i = 0; i < a.length; i++)
+    //     answer += a[i] * b[i];
+    // return answer;
+    
+    // 수업 풀이 (메서드 방식)
+    const answer = a.reduce((acc, cur, i) => {
+        return acc + (cur * b[i]);
+    }, 0)
     return answer;
 }
