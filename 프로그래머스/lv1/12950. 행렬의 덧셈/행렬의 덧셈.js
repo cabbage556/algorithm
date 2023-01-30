b0b3c2e7cfdea4d2e25b96c08ebed2cd2f12cb47
liberty556 <56855262+cabbage556@users.noreply.github.com>
2023-01-30 10:31:20 +0900

    [level 1] Title: 행렬의 덧셈, Time: 10.39 ms, Memory: 67.8 MB -BaekjoonHub

--- 프로그래머스/lv1/12950. 행렬의 덧셈/행렬의 덧셈.js
+++ 프로그래머스/lv1/12950. 행렬의 덧셈/행렬의 덧셈.js
@@ -1,12 +1,28 @@
 function solution(arr1, arr2) {
-    const answer = [];
+//     const answer = [];
 
+//     for (let i = 0; i < arr1.length; i++) {
+//       const temp = [];
+//       for (let j = 0; j < arr1[0].length; j++) {
+//         temp.push(arr1[i][j] + arr2[i][j]);
+//       }
+//       answer.push(temp);
+//     }
+    
+//     return answer;
+    
+    // 수업 풀이
+    const answer = [];
+    
     for (let i = 0; i < arr1.length; i++) {
-      const temp = [];
-      for (let j = 0; j < arr1[0].length; j++) {
-        temp.push(arr1[i][j] + arr2[i][j]);
-      }
-      answer.push(temp);
+        for (let j = 0; j < arr1[i].length; j++) {
+            const sum = arr1[i][j] + arr2[i][j];
+            
+            if (answer[i] === undefined)
+                answer[i] = [];
+            
+            answer[i][j] = sum;
+        }
     }
     
     return answer;
