378aca2051d6244e9206a74365628d941965b0ca
liberty556 <56855262+cabbage556@users.noreply.github.com>
2023-01-31 10:07:39 +0900

    [level 1] Title: 음양 더하기, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.js
+++ 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.js
@@ -1,9 +1,17 @@
 function solution(absolutes, signs) {
+    // let answer = 0;
+    // signs.forEach((sign, i) => {
+    //     sign ? answer += absolutes[i] : answer -= absolutes[i];
+    // })
+    // return answer;
+    
+    // 수업 풀이 (반복문)
     let answer = 0;
-    
-    signs.forEach((sign, i) => {
-        sign ? answer += absolutes[i] : answer -= absolutes[i];
-    })
-    
+    for (let i = 0; i < absolutes.length; i++) {
+        if (signs[i])
+            answer += absolutes[i];
+        else
+            answer -= absolutes[i];
+    }
     return answer;
 }
