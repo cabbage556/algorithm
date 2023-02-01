c33a413a9c0b31e2c26e00be5c20da73ecb4c5f6
liberty556 <56855262+cabbage556@users.noreply.github.com>
2023-02-01 10:06:51 +0900

    [level 1] Title: 2016년, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12901. 2016년/2016년.js
+++ 프로그래머스/lv1/12901. 2016년/2016년.js
@@ -1,11 +1,15 @@
 function solution(a, b) {
+    // 2016년 월별 일수
     const month = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
     let daySum = 0;
     
+    // 5월이면 4월까지의 일수를 더한다.
     for (let i = 0; i < a - 1; i++)
         daySum += month[i];
+    // 5월의 일수를 더한다.
     daySum += b;
     
+    // 5월 24일이 화요일인데, 5월 24일의 모든 일수를 7로 나누면 5가 남는다.
     if (daySum % 7 === 0) return "THU";
     else if (daySum % 7 === 1) return "FRI";
     else if (daySum % 7 === 2) return "SAT";
