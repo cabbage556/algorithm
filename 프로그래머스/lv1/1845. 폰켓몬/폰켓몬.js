5be5a83e89b2624d8de8e50c8ae777a483e46726
liberty556 <56855262+cabbage556@users.noreply.github.com>
2023-02-01 09:58:07 +0900

    [level 1] Title: 폰켓몬, Time: 0.32 ms, Memory: 33.9 MB -BaekjoonHub

--- 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.js
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.js
@@ -1,12 +1,12 @@
 function solution(nums) {
-    const selectCount = nums.length / 2;
-    const typeCount = (new Set(nums)).size;
+    const selectCount = nums.length / 2;     // 포켓몬 배열의 길이를 2로 나누기
+    const typeCount = (new Set(nums)).size;  // 포멧몬 배열에서 종류의 갯수를 얻기
     
-    console.log(selectCount, typeCount)
+    // console.log(selectCount, typeCount);
     
-    if (selectCount < typeCount)
+    if (selectCount < typeCount)      // N/2 보다 포켓몬의 종류가 더 큰 경우
         return selectCount;
-    else if (selectCount > typeCount)
+    else if (selectCount > typeCount) // N/2 보다 포켓몬의 종류가 더 작은 경우
         return typeCount;
     else
         return typeCount;
