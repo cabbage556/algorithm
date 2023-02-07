c6e15d0b6319747f6840503b800636e9875beb2e
liberty556 <56855262+cabbage556@users.noreply.github.com>
2023-02-07 10:12:49 +0900

    [level 1] Title: 폰켓몬, Time: 1.10 ms, Memory: 33.9 MB -BaekjoonHub

--- 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.js
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.js
@@ -1,13 +1,32 @@
 function solution(nums) {
-    const selectCount = nums.length / 2;     // 포켓몬 배열의 길이를 2로 나누기
-    const typeCount = (new Set(nums)).size;  // 포멧몬 배열에서 종류의 갯수를 얻기
+//     const selectCount = nums.length / 2;     // 포켓몬 배열의 길이를 2로 나눈 결과
+//     const typeCount = (new Set(nums)).size;  // 포멧몬 종류의 갯수
     
-    // console.log(selectCount, typeCount);
+//     return selectCount < typeCount
+//                             ? selectCount  // N/2 보다 포켓몬의 종류가 더 많은 경우
+//                             : typeCount;   // N/2 보다 포켓몬의 종류가 더 작은 경우
     
-    // N/2 보다 포켓몬의 종류가 더 큰 경우
-    if (selectCount < typeCount)
-        return selectCount;
-    // N/2 보다 포켓몬의 종류가 더 작은 경우
-    else
-        return typeCount;
+    
+//     const pokemon = {};
+//     const maxSelectCount = nums.length / 2;
+    
+//     for (const element of nums)
+//         pokemon[element] = 0;
+    
+//     const pokemonTypeCount = Object.keys(pokemon).length;  // 포켓몬 종류의 갯수
+    
+//     return pokemonTypeCount < maxSelectCount 
+//                             ? pokemonTypeCount
+//                             : maxSelectCount;
+    
+    // 수업 풀이
+    const answer = [];
+    
+    for (let i = 0; i < nums.length; i++) {
+        if (!answer.includes(nums[i]) && answer.length !== (nums.length / 2))
+            answer.push(nums[i]);
+    }
+    // answer => [3, 1] / [3, 2, 4] / [3, 2]
+    return answer.length;
+    
 }
