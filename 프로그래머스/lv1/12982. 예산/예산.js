43c6cb9699d7313bbcb116fc5c03799b36a5a0dc
liberty556 <56855262+cabbage556@users.noreply.github.com>
2023-02-10 09:24:51 +0900

    [level 1] Title: 예산, Time: 0.13 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12982. 예산/예산.js
+++ 프로그래머스/lv1/12982. 예산/예산.js
@@ -1,14 +1,5 @@
 function solution(d, budget) {
-  let answer = 0;
-  
-  d.sort((a, b) => a - b);
-  
-  for (let i = 0; i < d.length; i++) {
-    if (budget - d[i] >= 0) {
-      answer++;
-      budget -= d[i];
-    }
-  }
-  
-  return answer;
+    const answer = [];
+    
+    
 }
