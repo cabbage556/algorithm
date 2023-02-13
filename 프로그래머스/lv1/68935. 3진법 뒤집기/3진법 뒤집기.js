e5b6386b768ef91ae96d092cf6f732789dc0a0b0
liberty556 <56855262+cabbage556@users.noreply.github.com>
2023-02-14 08:49:40 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.16 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.js
+++ 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.js
@@ -3,15 +3,16 @@ function solution(n) {
     let answer = 0;
     let powerOfThree = 1;
     
+    // 3진수 변환 결과가 앞뒤 반전되도록 배열에 담기
     while (n > 0) {
         threes.push(n % 3);
         n = Math.floor(n / 3);
     }
     
-    powerOfThree = Math.pow(3, threes.length - 1);
+    powerOfThree = Math.pow(3, threes.length - 1);  // threes 배열의 길이가 4이면 첫 번째 요소의 3의 제곱수는 3이된다.
     for (let i = 0; i < threes.length; i++) {
-        answer += threes[i] * powerOfThree;
-        powerOfThree /= 3;
+        answer += threes[i] * powerOfThree;  // 10진법으로 표현하기
+        powerOfThree /= 3;                   // 배열 반복마다 3으로 나누기
     }
     
     return answer;
