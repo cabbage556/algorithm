992ba5a8af24e3ad9608194fe0a2f7517da1e974
liberty556 <56855262+cabbage556@users.noreply.github.com>
2023-02-16 10:31:58 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.13 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
@@ -1,33 +1,94 @@
 function solution(n ,arr1, arr2) {
-    const answer = [];
+//     const answer = [];
 
-    function getBinaryArr(arr) {
-        const tempArr = [];
+//     function getBinaryArr(arr) {
+//         const tempArr = [];
 
-        for (let el of arr) {
-          const temp = [];
+//         for (let el of arr) {
+//           const temp = [];
 
-          while (el > 0) {
-            temp.unshift((el % 2) + '');
-            el = Math.floor(el / 2);
-          }
-          tempArr.push(temp.join('').padStart(n, '0'));
-        }
+//           while (el > 0) {
+//             temp.unshift((el % 2) + '');
+//             el = Math.floor(el / 2);
+//           }
+//           tempArr.push(temp.join('').padStart(n, '0'));
+//         }
 
-        return tempArr;
-    }
-    const binary1 = getBinaryArr(arr1);
-    const binary2 = getBinaryArr(arr2);
+//         return tempArr;
+//     }
+//     const binary1 = getBinaryArr(arr1);
+//     const binary2 = getBinaryArr(arr2);
 
-    // console.log(binary1, binary2);
+//     // console.log(binary1, binary2);
 
-    for (let i = 0; i < binary1.length; i++) {
-        let temp = "";
-        for (let j = 0; j < binary1[i].length; j++) {
-          if (binary1[i][j] * 1 + binary2[i][j] * 1 === 0) temp += " ";
-          else temp += "#";
-        }
-        answer.push(temp);
-    }
+//     for (let i = 0; i < binary1.length; i++) {
+//         let temp = "";
+//         for (let j = 0; j < binary1[i].length; j++) {
+//           if (binary1[i][j] * 1 + binary2[i][j] * 1 === 0) temp += " ";
+//           else temp += "#";
+//         }
+//         answer.push(temp);
+//     }
+//     return answer;
+    
+    // 수업 풀이
+//     const answer = [];
+    
+//     // 두 배열의 길이가 같기 때문에 하나의 반복문 안에서 해결 가능
+//     for (let i = 0; i < arr1.length; i++) {
+//         answer[i] = ''; // undefined가 되지 않도록 빈문자열 할당
+        
+//         // 지도1, 2의 암호를 2진법으로 변환
+//         const map1 = arr1[i].toString(2).padStart(n, '0'); // 길이 n까지 '0'으로 채우기
+//         const map2 = arr2[i].toString(2).padStart(n, '0');
+        
+//         for (let j = 0; j < map1.length; j++) {
+//             // 벽이 되는 경우
+//             if (map1[j] === '1' || map2[j] === '1')
+//                 answer[i] += '#';
+//             // 공백이 되는 경우
+//             else
+//                 answer[i] += ' ';
+//         }
+//     }
+    
+//     return answer;
+    
+    // 수업 풀이
+    const answer = arr1.map((map1, i) => {
+        // 지도1, 2를 2진법으로 변환
+        map1 = map1.toString(2).padStart(n, '0');
+        const map2 = arr2[i].toString(2).padStart(n, '0');
+        
+        return map1.split('').reduce((acc, cur, j) => {
+            return acc + (cur === '1' || map2[j] === '1'
+                            ? '#'
+                            : ' '
+                        );
+        }, '');
+    });
     return answer;
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 }
