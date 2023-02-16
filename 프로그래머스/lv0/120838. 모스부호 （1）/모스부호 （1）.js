f52d3964feeb2ec2f3cc1f0f266b2f9937ac8895
liberty556 <56855262+cabbage556@users.noreply.github.com>
2023-02-17 08:34:05 +0900

    [level 0] Title: 모스부호 (1), Time: 0.13 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120838. 모스부호 （1）/모스부호 （1）.js
@@ -0,0 +1,18 @@
+const morse = { 
+    '.-':'a','-...':'b','-.-.':'c','-..':'d','.':'e','..-.':'f',
+    '--.':'g','....':'h','..':'i','.---':'j','-.-':'k','.-..':'l',
+    '--':'m','-.':'n','---':'o','.--.':'p','--.-':'q','.-.':'r',
+    '...':'s','-':'t','..-':'u','...-':'v','.--':'w','-..-':'x',
+    '-.--':'y','--..':'z'
+};
+
+function solution(letter) {
+    let answer = '';
+    letter = letter.split(' ');
+
+    for (const mor of letter) {
+    answer += morse[mor];
+    }
+
+    return answer;
+}
