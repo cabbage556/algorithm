94efacc3e44c0368a80d1175e1fbef8977fd679c
liberty556 <56855262+cabbage556@users.noreply.github.com>
2023-02-20 08:25:27 +0900

    [level 0] Title: 문자열 계산하기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120902. 문자열 계산하기/문자열 계산하기.js
@@ -0,0 +1,13 @@
+function solution(my_string) {
+    my_string = my_string.split(' ');
+    let answer = my_string[0] * 1;
+    
+    for (let i = 1; i < my_string.length; i+=2) {
+        if (my_string[i] === '+')
+            answer += my_string[i + 1] * 1;
+        else
+            answer -= my_string[i + 1] * 1;
+    }
+
+    return answer;
+}
