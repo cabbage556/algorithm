a1d87abe671bb798fa6d7951f17042da90a7f1b3
liberty556 <56855262+cabbage556@users.noreply.github.com>
2023-02-26 22:17:32 +0900

    [Bronze V] Title: 팩토리얼, Time: 124 ms, Memory: 9592 KB -BaekjoonHub

---
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+const input = fs.readFileSync("/dev/stdin").toString().trim();
+let answer = 1;
+for (let i = 2; i <= input; i++) {
+    answer *= i;
+}
+
+console.log(answer);
