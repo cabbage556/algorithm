4e45c3cdfad0b37f92dde089b04fdfe39ff91aa4
liberty556 <56855262+cabbage556@users.noreply.github.com>
2023-03-01 23:16:29 +0900

    [Bronze II] Title: Hashing, Time: 120 ms, Memory: 9316 KB -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.js
+++ 백준/Bronze/15829. Hashing/Hashing.js
@@ -6,10 +6,22 @@ const input = require("fs")
 const english = "abcdefghijklmnopqrstuvwxyz";
 const L = input[0] * 1;
 const str = input[1];
+const r = 31n;
+const M = 1234567891n;
 let answer = 0n;
 
 for (let i = 0; i < L; i++) {
-  answer += BigInt((english.indexOf(str[i]) + 1) * Math.pow(31, i));
+  const index = BigInt(english.indexOf(str[i]) + 1);
+  let temp = 1n;
+  let j = i;
+
+  while (j >= 1) {
+    temp *= r;
+    temp %= M;
+    j--;
+  }
+
+  answer += (index * temp) % M;
 }
 
-console.log((answer + "").slice(0));
+console.log(answer + "");
