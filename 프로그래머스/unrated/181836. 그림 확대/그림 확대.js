1ffa265ed2bc204cf4caf7e20ad31be9dd07ab6c
liberty556 <56855262+cabbage556@users.noreply.github.com>
2023-05-01 10:21:05 +0900

    [unrated] Title: 그림 확대, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181836. 그림 확대/그림 확대.js
@@ -0,0 +1,16 @@
+function solution(picture, k) {
+    const answer = [];
+    
+    for (let i = 0; i < picture.length; i++) {
+        let temp = '';
+        for (let j = 0; j < picture[i].length; j++) {
+            temp += picture[i][j].repeat(k);
+        }
+        
+        for (let l = 0; l < k; l++) {
+            answer.push(temp);
+        }
+    }
+    
+    return answer;
+}
