1136b73335060a5ef6f19db784be3aacc745659f
liberty556 <56855262+cabbage556@users.noreply.github.com>
2023-05-09 23:16:42 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12934. 정수 제곱근 판별/정수 제곱근 판별.js
+++ 프로그래머스/lv1/12934. 정수 제곱근 판별/정수 제곱근 판별.js
@@ -1,27 +1,36 @@
 function solution(n) {
-//     let answer = -1;
-    
-//     for (let i = 1; i <= n; i++) {
-//         if (i * i === n) {
-            // answer = (i + 1) * (i + 1);
-//             break;
-//         }
-//     }
-    
-    // return answer === -1 ? answer : answer * answer;
-    
-    // 수업 풀이
     let answer = -1;
     
-    // i * i === n, i는 n의 제곱근
-    for (let i = 1; i * i <= n; i++) {
-        if (i * i === n) {
-            // 제곱근을 찾은 경우
-            answer = i + 1;
-            return answer * answer;
+    for (let i = 1; i ** 2 <= n; i++) {
+        if (i ** 2 === n) {
+            answer = (i + 1) ** 2;
+            break;
         }
     }
     
-    // 제곱근을 찾지 못한 경우
     return answer;
+    
+    // 수업 풀이 (반복문 방식)
+//     let answer = -1;
+    
+//     // i * i === n, i는 n의 제곱근
+//     for (let i = 1; i * i <= n; i++) {
+//         if (i * i === n) {
+//             // 제곱근을 찾은 경우
+//             // answer = i + 1;
+//             // return answer * answer;
+            
+//             // return (i + 1) * (i + 1);
+            
+//             return (i + 1) ** 2;
+//         }
+//     }
+    
+    // // 제곱근을 찾지 못한 경우
+    // return answer;
+    
+    // 수업 풀이 (메서드 방식)
+    // let sqrt = Math.sqrt(n);
+    // return Number.isInteger(sqrt) ? (sqrt + 1) ** 2 : -1;
+    // return Number.isInteger(sqrt) ? Math.pow(sqrt + 1, 2) : -1;
 }
