06539756a1a5a38675c03f57375b908b95efc3b3
liberty556 <56855262+cabbage556@users.noreply.github.com>
2023-05-11 09:47:38 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 0.03 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.js
@@ -0,0 +1,13 @@
+function solution(seoul) {
+    return `김서방은 ${seoul.indexOf('Kim')}에 있다`;
+    
+    // 내 풀이
+    // const kim = seoul.findIndex(element => element === "Kim");
+    // const kim = seoul.indexOf("Kim");
+    // return `김서방은 ${kim}에 있다`;
+    
+    // 수업 풀이
+    // for (let i = 0; i < seoul.length; i++) {
+    //     if (seoul[i] === "Kim") return `김서방은 ${i}에 있다`;
+    // }
+}
