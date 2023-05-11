2a3adb3f9a0211ccb08346c3295557ab565469f7
liberty556 <56855262+cabbage556@users.noreply.github.com>
2023-05-11 10:18:19 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 0.08 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
+++ 프로그래머스/lv1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
@@ -1,21 +1,31 @@
 function solution(arr) {
-    // const min = Math.min(...arr);
-    // arr.splice(arr.indexOf(min), 1);
-    // return arr.length === 0 ? [-1] : arr;
-    
-    // 수업 풀이
-    const answer = [];
-    
-    // 1. 최소값 찾기
-    let min = arr[0];
-    arr.forEach(el => {
-        if (el < min) min = el;
-    });
-    
-    // 2. 최소값 제외한 숫자만 answer에 추가
-    arr.forEach(el => {
-        if (el !== min) answer.push(el);
-    });
+    const min = Math.min(...arr);
+    const answer = arr.filter(num => num !== min);
     
     return answer.length === 0 ? [-1] : answer;
+    
+    // 수업 풀이 (반복문 방식)
+//     const answer = [];
+    
+    // 1. 최소값 찾기
+//     let min = arr[0];
+//     arr.forEach(el => {
+//         if (el < min) min = el;
+//     });
+    
+    // 2. 최소값 제외한 숫자만 배열에 추가
+//     arr.forEach(el => {
+//         if (el !== min) answer.push(el);
+//     });
+    
+//     return answer.length === 0 ? [-1] : answer;
+    
+    // 수업 풀이 (메서드 방식)
+    // 1. 최소값 찾기
+    // const min = Math.min(...arr);
+    
+    // 2. 최소값을 제외한 숫자만 배열에 추가
+    // const answer = arr.filter(el => el !== min);
+    // return answer.length === 0 ? [-1] : answer;
+    
 }
