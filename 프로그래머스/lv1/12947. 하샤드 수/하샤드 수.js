d3b5907aa8b519e1bc1ebbcabde42da9164d374d
liberty556 <56855262+cabbage556@users.noreply.github.com>
2023-05-11 09:36:25 +0900

    [level 1] Title: 하샤드 수, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.js
+++ 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.js
@@ -1,6 +1,34 @@
 function solution(x) {
-    const sum = (x + '').split("")
-                        .reduce(((acc, cur) => acc * 1 + cur * 1), 0);
+    let sum = 0;
+    x = String(x);
     
-    return x % sum === 0;
+    for (let i = 0; i < x.length; i++) {
+        sum += x[i] * 1;
+    }
+    
+    return Number(x) % sum === 0;
+    
+    // [...String(x)] => 문자열 배열로 쪼개기
+    // const sum = [...String(x)]
+    //                         .reduce(((acc, cur) => acc * 1 + cur * 1), 0);
+    // const sum = (x + '').split("")
+    //                     .reduce(((acc, cur) => acc * 1 + cur * 1), 0);
+    // return x % sum === 0;
+    
+    // 수업 풀이 (반복문 방식)
+//     let sum = 0;
+//     x = String(x);
+    
+//     for (const str of x) sum += Number(str);
+    
+//     return x % sum === 0;
+    
+    // 수업 풀이 (메서드 방식)
+    // toString() 변수로 선언된 데이터에만 사용 가능
+    // const sum = x.toString()
+    //              .split("")
+    //              .reduce((acc, cur) => {
+    //                  return acc + Number(cur);
+    //              }, 0);
+    // return x % sum === 0;
 }
