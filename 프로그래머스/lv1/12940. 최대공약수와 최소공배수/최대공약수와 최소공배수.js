f34e9f2d9361b8ff4a68a762b52c56990ae18ce8
liberty556 <56855262+cabbage556@users.noreply.github.com>
2023-05-12 10:28:17 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 5.22 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.js
+++ 프로그래머스/lv1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.js
@@ -1,33 +1,41 @@
 function solution(n, m) {
-//     let gcd = 1;
-//     let lcm = 1;
+    // 유클리드 호제법: 최대공약수를 구하기 위한 알고리즘
+    // 1. a를 b로 나누었을 때 (a > b, 큰 수를 작은 수로 나누었을 때)
+    // 2. 나머지 값은 c
+    // 3. c가 0이라면, b가 최대공약수
+    // 4. c가 0이 아니라면, b를 c로 나눈다. (1번 과정)
     
-//     for (let i = 2; i <= Math.min(n, m); i++) {
-//         if (n % i === 0 && m % i === 0) gcd = i;
-//     }
+    let greater = Math.max(n, m);
+    let less = Math.min(n, m);
+    let remain = 0;
     
-//     lcm = n * m / gcd;
+    while (greater % less > 0) { // 큰 수와 작은 수의 나머지가 0일 때까지 반복
+        remain = greater % less; // 나머지
+        greater = less; // 큰 수에 작은 수를 할당
+        less = remain; // 작은 수에 나머지를 할당
+        console.log(greater, less, remain);
+    }
     
-//     return [gcd, lcm];
+    return [less, n * m / less];
     
     // 수업 풀이
     // 최대공약수: 두 개 이상의 수가 공통으로 가지는 약수 중에서 최댓값
     // 최소공배수: 두 개 이상의 수가 공통으로 가지는 배수 중에서 최솟값
     
-    let max = 0;  // 최대공약수
-    for (let i = 1; i <= m; i++) {
-        if (n % i === 0 && m % i === 0)
-            max = i;
-    }
+//     let max = 0;  // 최대공약수
+//     for (let i = 1; i <= m; i++) {
+//         if (n % i === 0 && m % i === 0)
+//             max = i;
+//     }
     
-    let min = 0;  // 최소공배수
-    // m 값으로 시작, m 값만큼 더함 => m의 배수
-    for (let i = m; i <= n * m; i += m) {
-        if (i % n === 0) {
-            min = i;
-            break;
-        }
-    }
+//     let min = 0;  // 최소공배수
+//     // m 값으로 시작, m 값만큼 더함 => m의 배수
+//     for (let i = m; i <= n * m; i += m) {
+//         if (i % n === 0) {
+//             min = i;
+//             break;
+//         }
+//     }
     
-    return [max, min];
+//     return [max, min];
 }
