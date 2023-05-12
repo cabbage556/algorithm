412a863938e21e378e13bac869eaeecb90315adf
liberty556 <56855262+cabbage556@users.noreply.github.com>
2023-05-12 10:00:46 +0900

    [level 1] Title: 행렬의 덧셈, Time: 9.25 ms, Memory: 67.1 MB -BaekjoonHub

--- 프로그래머스/lv1/12950. 행렬의 덧셈/행렬의 덧셈.js
+++ 프로그래머스/lv1/12950. 행렬의 덧셈/행렬의 덧셈.js
@@ -1,29 +1,39 @@
 function solution(arr1, arr2) {
+    const answer = [];
+    
+    for (let i = 0; i < arr1.length; i++) {
+        const sum = [];
+        
+        for (let j = 0; j < arr1[i].length; j++) {
+            sum.push(arr1[i][j] + arr2[i][j]);
+        }
+        
+        answer.push(sum);
+    }
+    
+    return answer;
+    
+    // 수업 풀이 (반복문 방식)
 //     const answer = [];
-
+    
 //     for (let i = 0; i < arr1.length; i++) {
-//       const temp = [];
-//       for (let j = 0; j < arr1[0].length; j++) {
-//         temp.push(arr1[i][j] + arr2[i][j]);
-//       }
-//       answer.push(temp);
+//         for (let j = 0; j < arr1[i].length; j++) {
+//             const sum = arr1[i][j] + arr2[i][j];
+            
+//             if (answer[i] === undefined)
+//                 answer[i] = [];
+            
+//             answer[i][j] = sum;
+//         }
 //     }
     
 //     return answer;
     
-    // 수업 풀이
-    const answer = [];
-    
-    for (let i = 0; i < arr1.length; i++) {
-        for (let j = 0; j < arr1[i].length; j++) {
-            const sum = arr1[i][j] + arr2[i][j];
-            
-            if (answer[i] === undefined)
-                answer[i] = [];
-            
-            answer[i][j] = sum;
-        }
-    }
-    
-    return answer;
+    // 수업 풀이 (메서드 방식)
+    // const answer = arr1.map((numArr, i) => {
+    //     return numArr.map((num, j) => {
+    //         return num + arr2[i][j];
+    //     });
+    // });
+    // return answer;
 }
