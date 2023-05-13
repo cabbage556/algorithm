a9801b511a7097d3279488125555bd78f510b8d7
liberty556 <56855262+cabbage556@users.noreply.github.com>
2023-05-14 08:26:08 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 47.46 ms, Memory: 82.9 MB -BaekjoonHub

--- 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
+++ 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -1,17 +1,27 @@
 function solution(arr) {
-    // 내 풀이
-    return arr.filter((element, idx) => {
-        return element !== arr[idx + 1];
-    })
-    
-    // 수업 풀이
     const answer = [];
     
     for (let i = 0; i < arr.length; i++) {
-        // if (arr[i] !== arr[i + 1]) answer.push(arr[i]);  // 1
-        if (answer[answer.length - 1] !== arr[i]) answer.push(arr[i]);
+        if (i === 0) {
+            answer.push(arr[i]);
+            continue;
+        }
+        
+        if (arr[i - 1] !== arr[i]) {
+            answer.push(arr[i]);
+        }
     }
     
     return answer;
+    
+    // 수업 풀이
+//     const answer = [];
+    
+//     for (let i = 0; i < arr.length; i++) {
+//         // if (arr[i] !== arr[i + 1]) answer.push(arr[i]);  // 1
+//         if (answer[answer.length - 1] !== arr[i]) answer.push(arr[i]);
+//     }
+    
+//     return answer;
     
 }
