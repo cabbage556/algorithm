679bba336727cd8487d875d1deb72bbccb05ba69
liberty556 <56855262+cabbage556@users.noreply.github.com>
2023-05-14 09:21:03 +0900

    [level 1] Title: 시저 암호, Time: 359.00 ms, Memory: 37.7 MB -BaekjoonHub

--- 프로그래머스/lv1/12926. 시저 암호/시저 암호.js
+++ 프로그래머스/lv1/12926. 시저 암호/시저 암호.js
@@ -1,27 +1,27 @@
-// const alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"; // 수업 풀이1 사용
+// 수업 풀이1 사용
+// const alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 // 수업 풀이2, 3 사용
 const lower = "abcdefghijklmnopqrstuvwxyz";
 const upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 function solution(s, n) {
-//     const lowCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-//     let answer = "";
+    let answer = '';
     
-//     for (let i = 0; i < s.length; i++) {
-//         if (s[i] === " ") {
-//             answer += s[i];
-//             continue;
-//         }
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] === ' ') {
+            answer += ' ';
+            continue;
+        }
         
-//         const afterIndex = (lowCase.indexOf(s[i].toLowerCase()) + n) % lowCase.length;
+        const str = upper.includes(s[i]) ? upper : lower;
         
-//         s[i] === s[i].toLowerCase()
-//             ? answer += lowCase[afterIndex]
-//             : answer += lowCase[afterIndex].toUpperCase();
-//     }
+        str.indexOf(s[i]) + n >= str.length
+            ? answer += str[str.indexOf(s[i]) + n - str.length]
+            : answer += str[str.indexOf(s[i]) + n];
+    }
     
-//     return answer;
+    return answer;
     
     // 수업 풀이1
 //     let answer = "";
@@ -62,17 +62,36 @@ function solution(s, n) {
 //     }
     
     // 수업 풀이3
-    const answer = s.split("")
-                    .reduce((acc, cur) => {
-                        const word = lower.includes(cur) ? lower : upper;
-                        let idx = word.indexOf(cur) + n;
+//     const answer = s.split("")
+//                     .reduce((acc, cur) => {
+//                         const word = lower.includes(cur) ? lower : upper;
+//                         let idx = word.indexOf(cur) + n;
                         
-                        if (idx > 25)
-                            idx -= 26;
+//                         if (idx > 25)
+//                             idx -= 26;
                         
-                        return acc + (
-                            cur === " " ? " " : word[idx]
-                        );
-                    }, "");
-    return answer;
+//                         return acc + (
+//                             cur === " " ? " " : word[idx]
+//                         );
+//                     }, "");
+//     return answer;
+    
+    // 수업 풀이4(아스키코드)
+    // charCodeAt: 주어진 문자의 유니코드 데이터(숫자)를 반환
+    // String.fromCharCode: 유니코드 데이터(숫자)를 문자열로 변환
+//     let answer = "";
+    
+//     for (let i = 0; i < s.length; i++) {
+//         if (s[i] === " ")
+//             answer += " ";
+//         else {
+//             let idx = s[i].charCodeAt() + n;  // 'A': 65, 'a': 97
+            
+//             if (idx > 122 || (idx > 90 && (idx - n) < 97)) // 소문자 체크, 대문자인데 대문자 범위를 넘어가지 않았는지 체크
+//                 idx -= 26;
+            
+//             answer += String.fromCharCode( idx );
+//         }
+//     }
+//     return answer;
 }
