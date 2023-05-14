cebb3f9f5a33eca90fe6f3ac24c04afb926d3774
liberty556 <56855262+cabbage556@users.noreply.github.com>
2023-05-14 09:01:03 +0900

    [level 1] Title: 예산, Time: 0.13 ms, Memory: 33 MB -BaekjoonHub

--- 프로그래머스/lv1/12982. 예산/예산.js
+++ 프로그래머스/lv1/12982. 예산/예산.js
@@ -1,5 +1,49 @@
 function solution(d, budget) {
-    const answer = [];
+    let answer = 0;
     
+    d = d.sort((a, b) => a - b);
     
+    for (let i = 0; i < d.length; i++) {
+        if (budget >= d[i]) {
+            budget -= d[i];
+            answer++;
+        } else {
+            break;
+        }
+    }
+    
+    return answer;
+    
+//     // 수업 풀이1
+//     let answer = 0;
+//     d.sort((a, b) => a - b);  // 오름차순 정렬
+    
+//     let sum = 0;
+//     for (let i = 0; i < d.length; i++) {
+//         sum += d[i];
+//         if (sum <= budget)
+//             answer++;
+//     }
+    
+//     return answer;
+    
+//     // 수업 풀이2
+//     let answer = 0;
+//     d.sort((a, b) => a - b);
+    
+//     while (budget - d[answer] >= 0) {
+//         budget -= d[answer];
+//         answer++;
+//     }
+    
+//     return answer;
+    
+    // 수업 풀이3
+    // const answer = d.sort((a, b) => a - b)
+    //                 .filter(money => {
+    //                     // 총 예산에서 지원금 차감
+    //                     budget -= money;
+    //                     return budget >= 0;
+    //                 });
+    // return answer.length;
 }
