f782880bbe586fdd3521f13e8c5ba10530d4dcb2
liberty556 <56855262+cabbage556@users.noreply.github.com>
2023-05-17 09:34:32 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 0.83 ms, Memory: 33.9 MB -BaekjoonHub

--- 프로그래머스/lv1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.js
+++ 프로그래머스/lv1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.js
@@ -1,12 +1,15 @@
 function solution(numbers) {
-//     const answer = [];
-
-//     while (numbers.length > 0) {
-//       const lastNum = numbers.pop();
-//       numbers.forEach(el => answer.push(lastNum + el));
-//     }
-
-//     return answer.filter((el, idx) => answer.indexOf(el) === idx).sort((a, b) => a - b);
+    const answer = [];
+    
+    for (let i = 0; i < numbers.length; i++) {
+        for (let j = i + 1; j < numbers.length; j++) {
+            answer.push(numbers[i] + numbers[j]);
+        }
+    }
+    
+    answer.sort((a, b) => a - b);
+    
+    return answer.filter((el, i) => answer.indexOf(el) === i);
     
     // 수업 풀이
 //     const answer = [];
@@ -36,14 +39,14 @@ function solution(numbers) {
 //     return Array.from(answer).sort((a, b) => a - b);
     
     // 수업 풀이 메서드 방식
-    const answer = new Set([]);
+//     const answer = new Set([]);
     
-    numbers.forEach((num1, i) => {
-        numbers.slice(i + 1).forEach((num2) => {
-            const sum = num1 + num2;
-            answer.add(sum);
-        });
-    });
+//     numbers.forEach((num1, i) => {
+//         numbers.slice(i + 1).forEach((num2) => {
+//             const sum = num1 + num2;
+//             answer.add(sum);
+//         });
+//     });
     
     return Array.from(answer).sort((a, b) => a - b);
 }
