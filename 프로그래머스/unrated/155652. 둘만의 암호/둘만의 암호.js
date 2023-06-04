1a6d3b9c5772f5180f9e19429d2f11d62c71a0b0
liberty556 <56855262+cabbage556@users.noreply.github.com>
2023-06-04 17:03:08 +0900

    [unrated] Title: 둘만의 암호, Time: 0.25 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/155652. 둘만의 암호/둘만의 암호.js
@@ -0,0 +1,29 @@
+function solution(s, skip, index) {
+    const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+    let answer = '';
+    let tempStr = '';
+    
+    // 문자열 s의 각 알파벳 읽기
+    for (let i = 0; i < s.length; i++) {
+        let checkIdx = alphabet.indexOf(s[i]) + 1;
+        let skipNum = 0;
+        
+        while (skipNum < index) {
+            if (checkIdx > 25) {
+                checkIdx -= 26;
+            }
+            
+            if (!skip.includes(alphabet[checkIdx])) {
+                tempStr += alphabet[checkIdx];
+                skipNum++;
+            }
+            
+            checkIdx++;
+        }
+        
+        answer += tempStr[tempStr.length - 1];
+        tempStr = '';
+    }
+    
+    return answer;
+}
