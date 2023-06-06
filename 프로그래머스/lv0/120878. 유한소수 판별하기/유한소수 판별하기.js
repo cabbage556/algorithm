714d4e76ebd9f9da37f91ef1991e2b1702bfd6a4
liberty556 <56855262+cabbage556@users.noreply.github.com>
2023-06-06 16:24:14 +0900

    [level 0] Title: 유한소수 판별하기, Time: 0.17 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120878. 유한소수 판별하기/유한소수 판별하기.js
@@ -0,0 +1,21 @@
+function solution(a, b) {
+    let gcd = 1;
+    
+    for (let i = 2; i <= a; i++) {
+        if (a % i === 0 && b % i === 0) {
+            gcd = i;
+        }
+    }
+    
+    b = b / gcd;
+    
+    while (b % 2 === 0) {
+        b /= 2;
+    }
+    
+    while (b % 5 === 0) {
+        b /= 5;
+    }
+    
+    return b === 1 ? 1 : 2;
+}
