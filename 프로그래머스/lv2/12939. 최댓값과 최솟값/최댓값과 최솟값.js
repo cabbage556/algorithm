cc477b81940ac6edcca0af0bf84073ff65929841
liberty556 <56855262+cabbage556@users.noreply.github.com>
2023-06-15 10:29:07 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.17 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
+++ 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
@@ -1,6 +1,17 @@
 function solution(s) {
-  const max = Math.max(...(s.split(' ')));
-  const min = Math.min(...(s.split(' ')));
-  
-  return `${min} ${max}`;
+    s = s.split(' ').map(Number);
+    let max = s[0];
+    let min = s[0];
+    
+    for (const value of s) {
+        if (value > max) {
+            max = value;
+        }
+        
+        if (value < min) {
+            min = value;
+        }
+    }
+    
+    return `${min} ${max}`;
 }
