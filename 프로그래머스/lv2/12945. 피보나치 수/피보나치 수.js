336889f411d3b4e71c00e0aef8b4d804a33ef559
liberty556 <56855262+cabbage556@users.noreply.github.com>
2023-06-16 10:43:31 +0900

    [level 2] Title: 피보나치 수, Time: 4.80 ms, Memory: 39 MB -BaekjoonHub

--- 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.js
+++ 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.js
@@ -1,31 +1,10 @@
-// 피보나치수: 0, 1, 1, 2, 3, ...
-// f(3) = f(2) + f(1)
-// f(2) = f(1) + f(0)
-
 function solution(n) {
-//     const answer = [0, 1];
-    
-//     for (let i = 0; i < n - 1; i++) {
-//         answer.push((answer[i] + answer[i + 1]) % 1234567);  // n이 커지면 오버플로우가 발생하므로 1234567로 나눠서 저장하기
-//     }
-    
-//     return answer[n];
-    
-    // 수업 풀이1
-    const answer = [];
-    
-    let prev = 0; // 0번째 숫자
-    let next = 1; // 1번째 숫자
-    let sum = 1;
+    const fibo = [0, 1];
     
     for (let i = 2; i <= n; i++) {
-        sum = (prev + next) % 1234567;
-        prev = next;
-        next = sum;
-        answer.push(sum);
+        // 1234567로 나눈 나머지로 저장
+        fibo.push((fibo[i - 2] + fibo[i - 1]) % 1234567);
     }
-    
-    return answer[n - 2];
-    
-    
+
+    return fibo[fibo.length - 1];
 }
