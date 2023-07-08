f9f6cd1b5d3f95c0a490652df8bf1e92d3caae78
liberty556 <56855262+cabbage556@users.noreply.github.com>
2023-07-08 14:25:24 +0900

    [level 2] Title: [3차] 압축, Time: 0.29 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv2/17684. ［3차］ 압축/［3차］ 압축.js
+++ 프로그래머스/lv2/17684. ［3차］ 압축/［3차］ 압축.js
@@ -1,47 +1,25 @@
 function solution(msg) {
-    // 사전 초기화 - 색인 번호 지정 -> String.fromCharCode(65) === 'A'
-    const dictionary = {};
-    let dictIndex = 1;
-    for (let i = 65; i <= 90; i++) {
-        dictionary[String.fromCharCode(i)] = dictIndex++;
+    const answer = []; // 압축
+
+    // 사전 초기화
+    const dictionary = new Map();
+    let dictionaryIndex = 1;
+    for (let i = 0; i < 26; i++) {
+        dictionary.set(String.fromCharCode(i + 65), dictionaryIndex++);
     }
-    
-    // 1
-    const answer = []; // 출력할 색인 번호 저장 배열
-    let str = '';      // 여러 글자를 임시 저장하는 변수
-    
+
+    let w = ""; // 현재 글자
     for (let i = 0; i < msg.length; i++) {
-        str += msg[i];
-        const next = str + msg[i + 1];
-        
-        if (!dictionary[next]) {             // 현재 글자와 바로 다음 글자를 합쳤을 때 사전에 존재하지 않는 경우
-            dictionary[next] = dictIndex++;  // 사전에 색인 번호와 함께 저장
-            answer.push(dictionary[str]);    // 사전에 이미 존재하는 현재 글자는 출력하기 위해 저장
-            str = '';                        // 저장 후 임시 저장 변수 초기화
+        w += msg[i];
+        const next = w + msg[i + 1];
+
+        // 사전에서 다음 글자가 없는 경우 등록
+        if (!dictionary.get(next)) {
+            dictionary.set(next, dictionaryIndex++); // 사전 등록
+            answer.push(dictionary.get(w)); // 압축
+            w = ""; // 현재 글자 초기화
         }
     }
-    
+
     return answer;
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    // 2
 }
