ecf8265db0d3ecc9f00b484733eaa46c32066db1
liberty556 <56855262+cabbage556@users.noreply.github.com>
2023-07-13 14:35:17 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 38.14 ms, Memory: 66.3 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,41 +1,21 @@
 function solution(participant, completion) {
-//     participant = participant.sort();
-//     completion = completion.sort();
-//     let answer = '';
-    
-//     for (let i = 0; i < participant.length; i++) {
-//         if (participant[i] !== completion[i]) {
-//             answer = participant[i];
-//             break;
-//         }
-//     }
-    
-//     return answer;
-    
-    // 수업 풀이 (잘못된 방법 => 데이터가 많아지는 경우 효율성 테스트 통과 못함)
-    // for (let i = 0; i < completion.length; i++) {
-    //     if (participant.includes(completion[i])) {
-    //         // participant에 completion 요소가 포함되므로 바로 배열에서 제거 가능
-    //         participant.splice( participant.indexOf(completion[i]), 1);
-    //     }
-    // }
-    // return participant[0];
-    
-    // 수업 풀이 (해싱)
-    const answer = {}; // "참가자 이름": 찾은 횟수
-    
-    for (let i = 0; i < participant.length; i++) {
-        answer[ participant[i] ] === undefined
-            ? answer[ participant[i] ] = 1
-            : answer[ participant[i] ] += 1;
+    const participantMap = new Map();
+
+    for (const person of participant) {
+        if (participantMap.get(person)) {
+            participantMap.set(person, participantMap.get(person) + 1);
+        } else {
+            participantMap.set(person, 1);
+        }
     }
-    
-    for (let i = 0; i < completion.length; i++) {
-        answer[ completion[i] ] -= 1;
+
+    for (const person of completion) {
+        participantMap.set(person, participantMap.get(person) - 1);
     }
-    
-    for (const key in answer) {
-        if (answer[key])
-            return key;
+
+    for (const [person, number] of participantMap) {
+        if (number === 1) {
+            return person;
+        }
     }
 }
