5f5eefa4355de93c0be46fde3090f7f3cbe476ef
cabbage556 <liberty556@gmail.com>
2023-07-26 09:24:08 +0900

    LeetCode: LeetCode 75 문제 풀이 392. Is Subsequence(투포인터 + while)

--- LeetCode/392. Is Subsequence/isSubsequence.js
+++ LeetCode/392. Is Subsequence/isSubsequence.js
@@ -19,7 +19,27 @@ var isSubsequence = function (s, t) {
     return sIndex === s.length;
 };
 
-console.log(isSubsequence("abc", "ahbgdc"));
-console.log(isSubsequence("axc", "ahbgdc"));
-console.log(isSubsequence("ab", "baab"));
-console.log(isSubsequence("", "ahbgdc"));
+// while 버전
+var isSubsequence2 = function (s, t) {
+    let sIndex = 0;
+    let tIndex = 0;
+
+    while (tIndex < t.length) {
+        if (s[sIndex] === t[tIndex]) {
+            sIndex++;
+        }
+        tIndex++;
+    }
+
+    return sIndex === s.length;
+};
+
+// console.log(isSubsequence("abc", "ahbgdc"));
+// console.log(isSubsequence("axc", "ahbgdc"));
+// console.log(isSubsequence("ab", "baab"));
+// console.log(isSubsequence("", "ahbgdc"));
+
+console.log(isSubsequence2("abc", "ahbgdc"));
+console.log(isSubsequence2("axc", "ahbgdc"));
+console.log(isSubsequence2("ab", "baab"));
+console.log(isSubsequence2("", "ahbgdc"));
