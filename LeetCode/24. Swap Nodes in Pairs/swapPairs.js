e7e87cacfe6dc4e6a3235181d6df5ce7e9825c08
cabbage556 <liberty556@gmail.com>
2023-09-10 16:28:13 +0900

    LeetCode: 24. Swap Nodes in Pairs - 재귀

---
+++ LeetCode/24. Swap Nodes in Pairs/swapPairs.js
@@ -0,0 +1,23 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var swapPairs = function (head) {
+  if (!head || !head.next) {
+    return head;
+  }
+
+  const nextNode = head.next;
+  head.next = swapPairs(head.next.next);
+  nextNode.next = head;
+  head = nextNode;
+
+  return head;
+};
