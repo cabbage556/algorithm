851ae66ad956b8d9f8a62dbd3953054d743c79a2
liberty556 <56855262+cabbage556@users.noreply.github.com>
2023-12-09 00:15:17 +0900

    [level unrated] Title: 왼쪽 오른쪽, Time: 0.03 ms, Memory: 75.7 MB -BaekjoonHub

--- 프로그래머스/unrated/181890. 왼쪽 오른쪽/왼쪽 오른쪽.java
+++ 프로그래머스/unrated/181890. 왼쪽 오른쪽/왼쪽 오른쪽.java
@@ -2,24 +2,15 @@ import java.util.Arrays;
 
 class Solution {
     public String[] solution(String[] str_list) {
-        int index = 0;
-        boolean rComesFirst = false;
-        
         for (int i = 0; i < str_list.length; i++) {
             String str = str_list[i];
             if (str.equals("l")) {
-                index = i;
-                break;
+                return Arrays.copyOfRange(str_list, 0, i);
             } else if (str.equals("r")) {
-                index = i;
-                rComesFirst = true;
-                break;
+                return Arrays.copyOfRange(str_list, i + 1, str_list.length);
             }
         }
         
-        if (rComesFirst) {
-            return Arrays.copyOfRange(str_list, index + 1, str_list.length);
-        }
-        return Arrays.copyOfRange(str_list, 0, index);
+        return new String[0];
     }
 }
