ab4f91849e270b57079f07ffffa325830b7e58a6
liberty556 <56855262+cabbage556@users.noreply.github.com>
2023-12-09 13:37:11 +0900

    [level unrated] Title: 문자열 여러 번 뒤집기, Time: 4.62 ms, Memory: 79.7 MB -BaekjoonHub

--- 프로그래머스/unrated/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.java
+++ 프로그래머스/unrated/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.java
@@ -3,15 +3,19 @@ class Solution {
         String[] split = my_string.split("");
         
         for (int[] query : queries) {
-            int s = query[0], e = query[1], j = 0;
-            for (int i = s; i <= (s + e) / 2; i++) {
-                String temp = split[i];
-                split[i] = split[e - j];
-                split[e - j] = temp;
-                j++;
-            }
+            reverse(split, query);
         }
         
         return String.join("", split);
+    }
+    
+    private static void reverse(String[] split, int[] query) {
+        int s = query[0], e = query[1], j = 0;
+        for (int i = s; i <= (s + e) / 2; i++) {
+            String temp = split[i];
+            split[i] = split[e - j];
+            split[e - j] = temp;
+            j++;
+        }
     }
 }
