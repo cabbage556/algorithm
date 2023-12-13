de43ee1549d76787503c496170faefc31467f92d
liberty556 <56855262+cabbage556@users.noreply.github.com>
2023-12-14 07:51:08 +0900

    [level unrated] Title: 짝수는 싫어요, Time: 1.93 ms, Memory: 74.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int n) {
+        List<Integer> result = new ArrayList<>();
+        
+        for (int i = 1; i <= n; i += 2) {
+            result.add(i);
+        }
+        
+        return result.stream().mapToInt(i -> i).toArray();
+    }
+}
