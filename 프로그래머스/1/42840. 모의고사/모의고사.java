ddf4eee837cff6637213356d94b86a0262871f96
liberty556 <56855262+cabbage556@users.noreply.github.com>
2023-12-16 10:38:26 +0900

    [level 1] Title: 모의고사, Time: 2.65 ms, Memory: 76.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+class Solution {
+    static int[] supo1 = {1, 2, 3, 4, 5};
+    static int[] supo2 = {2, 1, 2, 3, 2, 4, 2, 5};
+    static int[] supo3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+    
+    public int[] solution(int[] answers) {
+        int[] corrects = new int[3];
+        
+        for (int i = 0; i < answers.length; i++) {
+            int guess1 = supo1[i % supo1.length];
+            int guess2 = supo2[i % supo2.length];
+            int guess3 = supo3[i % supo3.length];
+            int answer = answers[i];
+            
+            if (guess1 == answer) {
+                corrects[0]++;
+            }
+            if (guess2 == answer) {
+                corrects[1]++;
+            }
+            if (guess3 == answer) {
+                corrects[2]++;
+            }
+        }
+        
+        int max = -1;
+        for (int correct : corrects) {
+            max = correct > max ? correct : max;
+        }
+        
+        List<Integer> result = new ArrayList<>();
+        for (int i = 0; i < corrects.length; i++) {
+            if (max == corrects[i]) {
+                result.add(i + 1);
+            }
+        }
+        return result.stream().mapToInt(i -> i).toArray();
+    }
+}
