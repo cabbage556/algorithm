490e6f1fbafb73905033f46c50624659dbb62b07
liberty556 <56855262+cabbage556@users.noreply.github.com>
2023-12-17 13:55:11 +0900

    [Silver V] Title: 영화감독 숌, Time: 324 ms, Memory: 86488 KB -BaekjoonHub

---
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
@@ -0,0 +1,27 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        int count = 0;
+        int shom = 0;
+        for (int i = 666; i < Integer.MAX_VALUE; i++) {
+            String str = String.valueOf(i);
+            if (str.contains("666")) {
+                count++;
+            }
+            if (count == N) {
+                shom = i;
+                break;
+            }
+        }
+
+        bw.write(String.valueOf(shom));
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
