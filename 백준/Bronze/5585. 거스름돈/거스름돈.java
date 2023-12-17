43ad0c0a8b585fc8c9e61057654e6715ebac263f
liberty556 <56855262+cabbage556@users.noreply.github.com>
2023-12-17 21:23:40 +0900

    [Bronze II] Title: 거스름돈, Time: 124 ms, Memory: 14140 KB -BaekjoonHub

--- 백준/Bronze/5585. 거스름돈/거스름돈.java
+++ 백준/Bronze/5585. 거스름돈/거스름돈.java
@@ -5,41 +5,20 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
+        int[] changes = {500, 100, 50, 10, 5, 1};
         int moneyLeft = 1000 - Integer.parseInt(br.readLine());
         int change = 0;
-        while (moneyLeft >= 500) {
-            moneyLeft -= 500;
-            change++;
-        }
 
-        while (moneyLeft >= 100) {
-            moneyLeft -= 100;
-            change++;
-        }
-
-        while (moneyLeft >= 50) {
-            moneyLeft -= 50;
-            change++;
-        }
-
-        while (moneyLeft >= 10) {
-            moneyLeft -= 10;
-            change++;
-        }
-
-        while (moneyLeft >= 5) {
-            moneyLeft -= 5;
-            change++;
-        }
-
-        while (moneyLeft >= 1) {
-            moneyLeft -= 1;
-            change++;
+        for (int i = 0; i < changes.length; i++) {
+            if (moneyLeft >= changes[i]) {
+                int num = moneyLeft / changes[i];
+                moneyLeft %= changes[i];
+                change += num;
+            }
         }
 
         bw.write(String.valueOf(change));
         bw.flush();
-        
         br.close();
         bw.close();
     }
