e448bcabead9069043845bf17f2ae82c9dd72856
liberty556 <56855262+cabbage556@users.noreply.github.com>
2023-12-24 01:49:46 +0900

    [Bronze IV] Title: 알파벳 개수, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/10808. 알파벳 개수/알파벳 개수.cc
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.cc
@@ -1,22 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-map<char, int> mp;
-string abc = "abcdefghijklmnopqrstuvwxyz";
+int cnt[26];
 string s;
 
 int main() {
 	cin >> s;
 	
-	for (int i = 0; i < s.size(); i++) {
-		if (mp.find(s[i]) == mp.end()) mp.insert({s[i], 1});
-		else mp[s[i]]++;
-	}
-	
-	for (int i = 0; i < abc.size(); i++) {
-		if (i == abc.size() - 1) cout << mp[abc[i]] << "\n";
-		else cout << mp[abc[i]] << " ";
-	}
+	for (char c : s) cnt[c - 97]++;
+	for (int i : cnt) cout << i << " ";
 
 	return 0;
 }
