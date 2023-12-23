981eefaac2277185b28be42954c4a0b21af60b94
liberty556 <56855262+cabbage556@users.noreply.github.com>
2023-12-24 01:19:42 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.cc
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.cc
@@ -1,39 +1,37 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int a[9];
+int sum;
 vector<int> v;
 vector<int> v2;
-int h;
 
-void print(vector<int> vec) {
-	for (int i : vec) cout << i << "\n";
-}
-
-void combi(int start, vector<int> vec) {
-	if (vec.size() == 7) {
-		int sum = accumulate(vec.begin(), vec.end(), 0);
-		if (sum == 100) {
-			print(vec);
-			exit(0);
+void solve() {
+	for (int i = 0; i < 9; i++) {
+		for (int j = i + 1; j < 9; j++) {
+			if (sum - a[i] - a[j] == 100) {
+				v.push_back(i);
+				v.push_back(j);
+				return;
+			}
 		}
 	}
-	
-	for (int i = start + 1; i < 9; i++) {
-		vec.push_back(v[i]);
-		combi(i, vec);
-		vec.pop_back();
-	}
-
-	return;
 }
 
 int main() {
 	for (int i = 0; i < 9; i++) {
-		cin >> h;
-		v.push_back(h);
+		cin >> a[i];
+		sum += a[i];
 	}
-	sort(v.begin(), v.end());
-	combi(-1, v2);
+	
+	solve();
+	
+	for (int i = 0; i < 9; i++) {
+		if (v[0] == i || v[1] == i) continue;
+		v2.push_back(a[i]);
+	}
+	sort(v2.begin(), v2.end());
+	for (int i : v2) cout << i << "\n";
 
 	return 0;
 }
