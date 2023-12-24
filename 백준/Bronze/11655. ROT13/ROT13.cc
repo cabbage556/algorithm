afa1acc0ddba5d3e6d4687ea46950904f87c9ab0
liberty556 <56855262+cabbage556@users.noreply.github.com>
2023-12-24 11:46:57 +0900

    [Bronze I] Title: ROT13, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/11655. ROT13/ROT13.cc
+++ 백준/Bronze/11655. ROT13/ROT13.cc
@@ -1,24 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-string s, ret;
+string s;
 
 int main() {
 	getline(cin , s);
 
 	for (int i = 0; i < s.size(); i++) {
 		if (s[i] >= 'a' && s[i] <= 'z') {
-			if (s[i] + 13 >= 'z' + 1) ret += (s[i] - 13);
-			else ret += (s[i] + 13);
+			if (s[i] + 13 >= 'z' + 1) s[i] -= 13;
+			else s[i] = (s[i] + 13);
 		} else if (s[i] >= 'A' && s[i] <= 'Z') {
-			if (s[i] + 13 >= 'Z' + 1) ret += (s[i] - 13);
-			else ret += (s[i] + 13);
-		} else {
-			ret += s[i];
+			if (s[i] + 13 >= 'Z' + 1) s[i] -= 13;
+			else s[i] = (s[i] + 13);
 		}
 	}
 	
-	cout << ret << "\n";
+	cout << s << "\n";
 
 	return 0;
 }
