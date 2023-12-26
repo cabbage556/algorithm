3af115b7521abe9ece5716c2784368ef26cce814
liberty556 <56855262+cabbage556@users.noreply.github.com>
2023-12-26 11:30:30 +0900

    [Silver IV] Title: 주몽, Time: 80 ms, Memory: 2156 KB -BaekjoonHub

--- 백준/Silver/1940. 주몽/주몽.cc
+++ 백준/Silver/1940. 주몽/주몽.cc
@@ -5,6 +5,9 @@ int N, M, num, ret;
 vector<int> v;
 
 int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
 	cin >> N >> M;
 	for (int i = 0; i < N; i++) {
 		cin >> num;
