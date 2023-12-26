7f914ad4f6dd2fcc6ed94127137c778744ef1e17
liberty556 <56855262+cabbage556@users.noreply.github.com>
2023-12-26 15:57:28 +0900

    [Silver IV] Title: 주몽, Time: 396 ms, Memory: 17340 KB -BaekjoonHub

---
+++ 백준/Silver/1940. 주몽/주몽.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        int M = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int cnt = 0;
+        for (int i = 0; i < N; i++) {
+            for (int j = i + 1; j < N; j++) {
+                if (arr[i] + arr[j] == M) cnt++;
+            }
+        }
+
+        bw.write(String.valueOf(cnt));
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
