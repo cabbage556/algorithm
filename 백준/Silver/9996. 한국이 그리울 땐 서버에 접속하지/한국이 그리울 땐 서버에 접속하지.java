066d72d608b3a840bd1d67f5198301ae7038d61f
liberty556 <56855262+cabbage556@users.noreply.github.com>
2023-12-28 10:12:47 +0900

    [Silver III] Title: 한국이 그리울 땐 서버에 접속하지, Time: 120 ms, Memory: 14268 KB -BaekjoonHub

--- 백준/Silver/9996. 한국이 그리울 땐 서버에 접속하지/한국이 그리울 땐 서버에 접속하지.java
+++ 백준/Silver/9996. 한국이 그리울 땐 서버에 접속하지/한국이 그리울 땐 서버에 접속하지.java
@@ -7,39 +7,25 @@ public class Main {
 
         int n = Integer.parseInt(br.readLine());
         String pt = br.readLine();
+
         int aster = pt.indexOf('*');
         String first = pt.substring(0, aster);
         String second = pt.substring(aster + 1);
 
         for (int i = 0; i < n; i++) {
             String s = br.readLine();
-            
+
             if (first.length() + second.length() > s.length()) {
                 bw.write("NE\n");
                 continue;
             }
-            
-            StringBuilder sb = new StringBuilder();
-            for (int j = 0; j < first.length(); j++) {
-                sb.append(s.charAt(j));
-            }
 
-            if (!first.contentEquals(sb)) {
+            if (first.equals(s.substring(0, first.length()))
+                    && second.equals(s.substring(s.length() - second.length()))) {
+                bw.write("DA\n");
+            } else {
                 bw.write("NE\n");
-                continue;
             }
-
-            sb.delete(0, sb.length());
-            for (int j = s.length() - second.length(); j < s.length(); j++) {
-                sb.append(s.charAt(j));
-            }
-
-            if (!second.contentEquals(sb)) {
-                bw.write("NE\n");
-                continue;
-            }
-
-            bw.write("DA\n");
         }
 
         bw.flush();
