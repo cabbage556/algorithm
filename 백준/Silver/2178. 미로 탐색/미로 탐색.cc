d59b74f7920d48248f7483f3b761b25e36e20d20
liberty556 <56855262+cabbage556@users.noreply.github.com>
2024-01-02 13:33:13 +0900

    [Silver I] Title: 미로 탐색, Time: 0 ms, Memory: 2108 KB -BaekjoonHub

---
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.cc
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, m;
+string s;
+int miro[104][104], visited[104][104];
+
+int dy[] = {-1, 0, 1, 0};
+int dx[] = {0, 1, 0, -1};
+
+void bfs(int y, int x) {
+	queue<pair<int, int>> q;
+	q.push({y, x});
+	visited[y][x] = 1;
+	
+	while (q.size()) {
+		tie(y, x) = q.front();
+		q.pop();
+
+		for (int i = 0; i < 4; i++) {
+			int ny = y + dy[i];
+			int nx = x + dx[i];
+			
+			if (ny < 0 || nx < 0 || ny > n || nx > m) continue;
+			if (miro[ny][nx] == 0) continue;
+			if (visited[ny][nx] > 0) continue;
+
+			visited[ny][nx] = visited[y][x] + 1;
+			q.push({ny, nx});
+		}
+	}
+}
+
+int main() {
+	cin >> n >> m;
+	for (int i = 1; i <= n; i++) {
+		cin >> s;
+		for (int j = 1; j <= m; j++) {
+			miro[i][j] = s[j - 1] - '0';
+		}
+	}
+	
+	bfs(1, 1);
+	cout << visited[n][m] << "\n";
+	
+	return 0;
+}
