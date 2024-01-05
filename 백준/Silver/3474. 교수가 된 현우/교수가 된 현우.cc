56883afb8178ddd4ecf880f7748a8756459616dd
liberty556 <56855262+cabbage556@users.noreply.github.com>
2024-01-05 16:49:13 +0900

    [Silver III] Title: 교수가 된 현우, Time: 20 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/3474. 교수가 된 현우/교수가 된 현우.cc
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int t, n;
+
+int main() { 
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> t;
+	for (int i = 0; i < t; i++) {
+		cin >> n;
+		
+		int cnt = 0;
+		for (int j = 5; j <= n; j *= 5) {
+			cnt += n / j;
+		}
+		cout << cnt << "\n";
+	}
+
+	return 0;
+}
