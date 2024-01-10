72e6e41a829713110287b5e40109489e75985218
liberty556 <56855262+cabbage556@users.noreply.github.com>
2024-01-10 20:34:54 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 144 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.cc
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+string s;
+
+int main() {
+	while (true) {
+		getline(cin, s);
+		if (s == ".") break;
+		
+		stack<char> stk;
+		bool balanced = true;
+		for (int i = 0; i < s.size(); i++) {
+			char c = s[i];
+			if (c == '(' || c == '[') {
+				stk.push(c);
+			} else if (c == ')') {
+				if (!stk.empty() && stk.top() == '(') stk.pop();
+				else {
+					balanced = false;
+					break;
+				}
+			} else if (c == ']') {
+				if (!stk.empty() && stk.top() == '[') stk.pop();
+				else {
+					balanced = false;
+					break;
+				}
+			}
+		}
+		
+		if (!balanced || !stk.empty()) cout << "no\n";
+		else cout << "yes\n";
+	}
+	return 0;
+}
