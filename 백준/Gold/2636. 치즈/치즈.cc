b69309d98f9af86c600064e9d6efb5efc9cd16a5
liberty556 <56855262+cabbage556@users.noreply.github.com>
2024-01-11 20:58:54 +0900

    [Gold IV] Title: 치즈, Time: 0 ms, Memory: 2076 KB -BaekjoonHub

--- 백준/Gold/2636. 치즈/치즈.cc
+++ 백준/Gold/2636. 치즈/치즈.cc
@@ -6,19 +6,28 @@ bool visited[104][104];
 int dy[] = {-1, 0, 1, 0}, dx[] = {0, 1, 0, -1};
 vector<pair<int, int>> v;
 
-void dfs(int y, int x) {
+void bfs(int y, int x) {
 	visited[y][x] = true;
-	if (bd[y][x] == 1) {
-		v.push_back({y, x});
-		return;
-	}
-	
-	for (int i = 0; i < 4; i++) {
-		int ny = y + dy[i];
-		int nx = x + dx[i];
-		if (ny < 0 || ny >= n || nx < 0 || nx >= m || visited[ny][nx]) continue;
+	queue<pair<int, int>> q;
+	q.push({y, x});
 
-		dfs(ny, nx);
+	while (q.size()) {
+		tie(y, x) = q.front();
+		q.pop();
+
+		if (bd[y][x] == 1) {
+			v.push_back({y, x});
+			continue;
+		}
+		
+		for (int i = 0; i < 4; i++) {
+			int ny = y + dy[i];
+			int nx = x + dx[i];
+			if (ny < 0 || nx < 0 || ny >= n || nx >= m || visited[ny][nx]) continue;
+
+			visited[ny][nx] = true;
+			q.push({ny, nx});
+		}
 	}
 	return;
 }
@@ -34,7 +43,8 @@ int main() {
 	while (true) {
 		fill(&visited[0][0], &visited[0][0] + 104 * 104, false);
 		v.clear();
-		dfs(0, 0);
+
+		bfs(0, 0);
 		left_cheeze = v.size();
 		for (auto it : v) {
 			bd[it.first][it.second] = 0;
