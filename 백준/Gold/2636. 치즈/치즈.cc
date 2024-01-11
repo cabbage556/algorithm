5633008e69bd9c0c111539ac1eb02d6f0af4086b
liberty556 <56855262+cabbage556@users.noreply.github.com>
2024-01-11 20:52:31 +0900

    [Gold IV] Title: 치즈, Time: 0 ms, Memory: 2132 KB -BaekjoonHub

---
+++ 백준/Gold/2636. 치즈/치즈.cc
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, m, bd[104][104], total_hour, left_cheeze;
+bool visited[104][104];
+int dy[] = {-1, 0, 1, 0}, dx[] = {0, 1, 0, -1};
+vector<pair<int, int>> v;
+
+void dfs(int y, int x) {
+	visited[y][x] = true;
+	if (bd[y][x] == 1) {
+		v.push_back({y, x});
+		return;
+	}
+	
+	for (int i = 0; i < 4; i++) {
+		int ny = y + dy[i];
+		int nx = x + dx[i];
+		if (ny < 0 || ny >= n || nx < 0 || nx >= m || visited[ny][nx]) continue;
+
+		dfs(ny, nx);
+	}
+	return;
+}
+
+int main() {
+	cin >> n >> m;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			cin >> bd[i][j];
+		}
+	}
+	
+	while (true) {
+		fill(&visited[0][0], &visited[0][0] + 104 * 104, false);
+		v.clear();
+		dfs(0, 0);
+		left_cheeze = v.size();
+		for (auto it : v) {
+			bd[it.first][it.second] = 0;
+		}
+		
+		bool cheeze_exists = false;
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++) {
+				if (bd[i][j] != 0) cheeze_exists = true;
+			}
+		}
+		total_hour++;
+		if (!cheeze_exists) break;
+	}
+	cout << total_hour << "\n" << left_cheeze << "\n";
+
+	return 0;
+}
