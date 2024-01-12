c21e0c9e2b50d656271c435391c6fdcb225d93f4
liberty556 <56855262+cabbage556@users.noreply.github.com>
2024-01-12 21:16:49 +0900

    [Gold IV] Title: 오큰수, Time: 216 ms, Memory: 14328 KB -BaekjoonHub

--- 백준/Gold/17298. 오큰수/오큰수.cc
+++ 백준/Gold/17298. 오큰수/오큰수.cc
@@ -8,29 +8,19 @@ stack<pair<int, int>> stk;
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
+	fill(nge, nge + 1000004, -1);
 
 	cin >> n;
 	for (int i = 1; i <= n; i++) {
 		cin >> el;
-		if (stk.empty()) stk.push({i, el});
-		else if (stk.top().second >= el) stk.push({i, el});
-		else {
-			while (!stk.empty() && stk.top().second < el) {
-				tie(idx, num) = stk.top();
-				stk.pop();
-				
-				nge[idx] = el;
-			}
-			stk.push({i, el});
+		while (!stk.empty() && stk.top().second < el) {
+			tie(idx, num) = stk.top();
+			stk.pop();
+			nge[idx] = el;
 		}
+		stk.push({i, el});
 	}
 	
-	while (!stk.empty()) {
-		tie(idx, num) = stk.top();
-		stk.pop();
-
-		nge[idx] = -1;
-	}
 	for (int i = 1; i <= n; i++) {
 		cout << nge[i] << " ";
 	}
