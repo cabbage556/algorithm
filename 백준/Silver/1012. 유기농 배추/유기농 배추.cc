a0ab93b4f4f48198b602bf615e6254b64b24c132
liberty556 <56855262+cabbage556@users.noreply.github.com>
2024-01-13 20:48:50 +0900

    [Silver II] Title: 유기농 배추, Time: 4 ms, Memory: 2036 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.cc
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.cc
@@ -1,50 +1,50 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int t, m, n, k, y, x;
-int ret;
-int field[54][54], visited[54][54];
-
-int dy[] = {-1, 0, 1, 0};
-int dx[] = {0, 1, 0, -1};
+int t, m, n, k, x, y;
+int farm[54][54];
+bool visited[54][54];
+int dy[] = {-1, 0, 1, 0}, dx[] = {0, 1, 0, -1};
 
 void dfs(int y, int x) {
-	visited[y][x] = 1;
+	visited[y][x] = true;
 
 	for (int i = 0; i < 4; i++) {
 		int ny = y + dy[i];
 		int nx = x + dx[i];
-		if (ny < 0 || nx < 0 || ny >= n || nx >= m || field[ny][nx] == 0) continue;
-		if (visited[ny][nx]) continue;
-		
+		if (ny < 0 || ny > n || nx < 0 || nx > m || visited[ny][nx] || farm[ny][nx] == 0) continue;
+
 		dfs(ny, nx);
 	}
+}
+	
+int solve() {
+	int cnt = 0;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			if (!visited[i][j] && farm[i][j] == 1) {
+				dfs(i, j);
+				cnt++;
+			}
+		}
+	}
+	return cnt;
 }
 
 int main() {
 	cin >> t;
-
 	for (int i = 0; i < t; i++) {
 		cin >> m >> n >> k;
-
+		
 		for (int j = 0; j < k; j++) {
 			cin >> x >> y;
-			field[y][x] = 1;
+			farm[y][x] = 1;
 		}
 		
-		for (int j = 0; j < n; j++) {
-			for (int l = 0; l < m; l++) {
-				if (field[j][l] == 1 && visited[j][l] == 0) {
-					dfs(j, l);
-					ret++;
-				}
-			}
-		}
-		cout << ret << "\n";
-		memset(field, 0, sizeof(field));
-		memset(visited, 0, sizeof(visited));
-		ret = 0;
+		cout << solve() << "\n";
+		fill(&visited[0][0], &visited[0][0] + 54 * 54, false);
+		fill(&farm[0][0], &farm[0][0] + 54 * 54, 0);
 	}
-	
+
 	return 0;
 }
