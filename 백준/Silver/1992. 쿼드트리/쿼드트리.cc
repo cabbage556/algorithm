94acf88ceb82c7abc46661145fcf8446d09fa8a0
liberty556 <56855262+cabbage556@users.noreply.github.com>
2024-01-13 23:29:13 +0900

    [Silver I] Title: 쿼드트리, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/1992. 쿼드트리/쿼드트리.cc
+++ 백준/Silver/1992. 쿼드트리/쿼드트리.cc
@@ -2,38 +2,35 @@
 using namespace std;
 
 int n;
-string s;
-char a[70][70];
+char img[70][70];
 
 string quad(int y, int x, int size) {
-	if (size == 1) return string(1, a[y][x]);
-	
-	char b = a[y][x];
 	string ret = "";
+	char first = img[y][x];
 	for (int i = y; i < y + size; i++) {
 		for (int j = x; j < x + size; j++) {
-			if (b != a[i][j]) {
+			if (first != img[i][j]) {
 				ret += "(";
-				ret += quad(y, x, size / 2);
-				ret += quad(y, x + size / 2, size / 2);
-				ret += quad(y + size / 2, x, size / 2);
-				ret += quad(y + size / 2, x + size / 2, size / 2);
+				ret += quad(y, x, size / 2); // 왼쪽 위 절반씩
+				ret += quad(y, x + size / 2, size / 2); // 오른쪽 위 절반씩
+				ret += quad(y + size / 2, x, size / 2); // 왼쪽 아래 절반씩
+				ret += quad(y + size / 2, x + size / 2, size / 2); // 오른쪽 아래 절반씩
 				ret += ")";
 				return ret;
 			}
 		}
 	}
-	return string(1, a[y][x]);
+
+	return string(1, first);
 }
 
 int main() {
 	cin >> n;
 	for (int i = 0; i < n; i++) {
-		cin >> s;
 		for (int j = 0; j < n; j++) {
-			a[i][j] = s[j];
+			scanf(" %c", &img[i][j]);
 		}
 	}
-	
 	cout << quad(0, 0, n) << "\n";
+	return 0;
 }
