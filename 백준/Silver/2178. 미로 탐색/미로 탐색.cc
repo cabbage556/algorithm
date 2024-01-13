c73224c19473e806b55fa1b120960129f7ad4392
liberty556 <56855262+cabbage556@users.noreply.github.com>
2024-01-13 20:28:58 +0900

    [Silver I] Title: 미로 탐색, Time: 0 ms, Memory: 2104 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.cc
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.cc
@@ -2,36 +2,31 @@
 using namespace std;
 
 int n, m;
-int miro[104][104], visited[104][104];
-
-int dy[] = {-1, 0, 1, 0};
-int dx[] = {0, 1, 0, -1};
+int miro[104][104], cnt[104][104];
+int dy[] = {-1, 0, 1, 0}, dx[] = {0, 1, 0, -1};
 
 void bfs(int y, int x) {
+	cnt[y][x] = 1;
 	queue<pair<int, int>> q;
 	q.push({y, x});
-	visited[y][x] = 1;
 	
-	while (q.size()) {
+	while (!q.empty()) {
 		tie(y, x) = q.front();
 		q.pop();
 
 		for (int i = 0; i < 4; i++) {
 			int ny = y + dy[i];
 			int nx = x + dx[i];
-			
-			if (ny < 0 || nx < 0 || ny > n || nx > m) continue;
-			if (miro[ny][nx] == 0) continue;
-			if (visited[ny][nx] > 0) continue;
+			if (ny < 0 || ny > n || nx < 0 || nx > m || cnt[ny][nx] > 0 || miro[ny][nx] == 0) continue;
 
-			visited[ny][nx] = visited[y][x] + 1;
+			cnt[ny][nx] = cnt[y][x] + 1;
 			q.push({ny, nx});
 		}
 	}
 }
 
 int main() {
-	scanf("%d %d", &n, &m);
+	cin >> n >> m;
 	for (int i = 1; i <= n; i++) {
 		for (int j = 1; j <= m; j++) {
 			scanf("%1d", &miro[i][j]);
@@ -39,7 +34,7 @@ int main() {
 	}
 	
 	bfs(1, 1);
-	cout << visited[n][m] << "\n";
-	
+	cout << cnt[n][m] << "\n";
+
 	return 0;
 }
