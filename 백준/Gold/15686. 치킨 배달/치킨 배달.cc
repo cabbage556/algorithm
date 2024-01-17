71488bb3fe3100dd4433bcdc9ca12eec927097fc
liberty556 <56855262+cabbage556@users.noreply.github.com>
2024-01-17 15:44:54 +0900

    [Gold V] Title: 치킨 배달, Time: 4 ms, Memory: 2036 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.cc
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.cc
@@ -5,7 +5,7 @@ typedef pair<int, int> pii;
 
 int n, m;
 int city[54][54];
-vector<pii> c;
+vector<pii> c, h;
 
 int getDistance(pii home, pii chicken) {
 	return abs(home.first - chicken.first) + abs(home.second - chicken.second);
@@ -13,17 +13,13 @@ int getDistance(pii home, pii chicken) {
 
 int solve(vector<int> v) {
 	int dist_sum = 0;
-	for (int i = 1; i <= n; i++) {
-		for (int j = 1; j <= n; j++) {
-			int dist = 100;
-			if (city[i][j] == 1) {
-				for (int k : v) {
-					dist = min(dist, getDistance({i, j}, c[k]));
-					if (dist == 1) break;
-				}
-				dist_sum += dist;
-			}
+	for (pii pos : h) {
+		int dist = 100;
+		for (int i : v) {
+			dist = min(dist, getDistance(pos, c[i]));
+			if (dist == 1) break;
 		}
+		dist_sum += dist;
 	}
 	return dist_sum;
 }
@@ -47,7 +43,8 @@ int main() {
 	for (int i = 1; i <= n; i++) {
 		for (int j = 1; j <= n; j++) {
 			cin >> city[i][j];
-			if (city[i][j] == 2) c.push_back({i, j});
+			if (city[i][j] == 1) h.push_back({i, j});
+			else if (city[i][j] == 2) c.push_back({i, j});
 		}
 	}
 	
