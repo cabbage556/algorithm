591983871fb6ed4948542534e85f1891721e11f3
liberty556 <56855262+cabbage556@users.noreply.github.com>
2024-01-23 13:32:59 +0900

    [level 2] Title: 행렬의 곱셈, Time: 9.59 ms, Memory: 10.6 MB -BaekjoonHub

--- 프로그래머스/2/12949. 행렬의 곱셈/README.md
+++ 프로그래머스/2/12949. 행렬의 곱셈/README.md
@@ -1,10 +1,10 @@
 # [level 2] 행렬의 곱셈 - 12949 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12949?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12949) 
 
 ### 성능 요약
 
-메모리: 10.5 MB, 시간: 12.75 ms
+메모리: 10.6 MB, 시간: 9.59 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 1월 1일 17:27:13
+2024년 1월 2일 13:32:57
 
 ### 문제 설명
 
--- 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.py
+++ 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.py
@@ -1,15 +1,11 @@
 def solution(arr1, arr2):
-    # 곱셈 결과 행렬 생성
-    #		(3 X 2) x (2 X 2) => (3 X 2)
-    ret = [[0] * len(arr2[0]) for _ in range(len(arr1))]
-
-    # arr1의 행의 길이만큼
-    for i in range(len(arr1)):
-        # arr2의 열의 길이만큼
-        for j in range(len(arr2[0])):
-            # arr1의 열의 길이만큼
-            for k in range(len(arr1[0])):
-                ret[i][j] += arr1[i][k] * arr2[k][j];
+    r1, c1 = len(arr1), len(arr1[0])
+    r2, c2 = len(arr2), len(arr2[0])
+    ret = [[0] * c2 for _ in range(r1)]
     
-    # 곱셈 결과 행렬 리턴
+    for i in range(r1):
+        for j in range(c2):
+            for k in range(c1):
+                ret[i][j] += arr1[i][k] * arr2[k][j]
+    
     return ret
