429ba582cd25929cdb257527f457fb4c8d7e6bdd
cabbage556 <liberty556@gmail.com>
2024-04-26 14:02:14 +0900

    그리디 연습문제 복습: 큰 수의 법칙

--- 이코테/그리디/큰 수의 법칙1.py
+++ 이코테/그리디/큰 수의 법칙1.py
@@ -1,18 +1,36 @@
-n, m, k = map(int, input().split())
-nums = list(map(int, input().split()))
+"""
+큰 수의 법칙
+    배열의 원소들을 M번 더해 최댓값을 만드는 법칙
+    단, 특정 인덱스 값을 연속 K번 초과해 더할 수 없음
+    단, 서로 다른 인덱스에 해당하는 수가 같은 경우에도 서로 다른 것으로 간주함
+"""
 
+# n : 배열의 크기
+# m : 숫자가 더해지는 횟수
+# k : 연속 덧셈 최대 횟수
+n, m, k = map(int, input().split())
+
+# 배열
+nums = list(map(int, input().split()))
 nums.sort()
-ret = 0
+
+first = nums[-1]  # 가장 큰 수
+second = nums[-2]  # 두 번째로 큰 수
+res = 0
+
 while True:
+    # 가장 큰 수 k번 더하기
     for _ in range(k):
         if m == 0:
             break
-        ret += nums[-1]
+        res += first
         m -= 1
 
     if m == 0:
         break
-    ret += nums[-2]
+
+    # 두 번째로 큰 수 1번 더하기
+    res += second
     m -= 1
 
-print(ret)
+print(res)
--- 이코테/그리디/큰 수의 법칙2.py
+++ 이코테/그리디/큰 수의 법칙2.py
@@ -1,11 +1,19 @@
+# n : 배열의 크기
+# m : 숫자가 더해지는 횟수
+#   m의 크기가 100억 이상인 경우 수학적으로 해결할 수 있음
+# k : 연속 덧셈 최대 횟수
 n, m, k = map(int, input().split())
 nums = list(map(int, input().split()))
-
 nums.sort()
 
-# 가장 큰 수의 등장 횟수 = 반복 수열 등장 횟수 * k + m이 k+1로 나누어 떨어지지 않을 때의 나머지
-cnt = int(m / (k + 1)) * k + m % (k + 1)
+first = nums[-1]
+second = nums[-2]
 
-ret = nums[-1] * cnt
-ret += nums[-2] * (m - cnt)
-print(ret)
+# 가장 큰 수의 등장 횟수 = 반복 수열 길이 * k + m이 k+1로 나누어 떨어지지 않을 때의 나머지
+cnt = int(m / (k + 1)) * k  # 가장 큰 수와 두 번째로 큰 수가 특정 수열 형태로 반복됨(가장 큰 수 k개 + 두 번째로 큰 수 1개)
+cnt += m % (k + 1)  # m이 k+1로 나누어 떨어지지 않는 경우에는 가장 큰 수가 그 나머지만큼 등장함
+
+res = cnt * first  # 가장 큰 수 더하기
+res += (m - cnt) * second  # 두 번째로 큰 수 더하기
+
+print(res)
