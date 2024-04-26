4757755aec758fb369bec904a26eaf738d6b165e
cabbage556 <liberty556@gmail.com>
2024-04-26 14:34:07 +0900

    그리디 연습문제 복습: 1이 될 때까지

--- 이코테/그리디/1이 될 때까지2.py
+++ 이코테/그리디/1이 될 때까지2.py
@@ -1,19 +1,28 @@
-n, k = map(int, input().split())
-ret = 0
+"""
+n을 k로 최대한 많이 나누는 것이 최적의 해를 보장함
+반복 과정
+1. n이 k의 배수가 될 때까지 1씩 빼기
+2. n을 k로 나누기
+"""
 
-# n이 k 이상이면 k로 계속 나누기
-while n >= k:
-    # n이 k로 나누어 떨어지지 않으면 n에서 1씩 빼기
+n, k = map(int, input().split())
+res = 0
+
+# n이 k 이상이면 반복
+while n >= k    :
+
+    # n이 k의 배수가 될 때까지 1씩 빼기
     while n % k != 0:
         n -= 1
-        ret += 1
-    # k로 나누기
+        res += 1
+
+    # n을 k로 나누기
     n //= k
-    ret += 1
+    res += 1
 
 # 남은 수에 대해 1씩 빼기
 while n > 1:
     n -= 1
-    ret += 1
+    res += 1
 
-print(ret)
+print(res)
--- 이코테/그리디/1이 될 때까지3.py
+++ 이코테/그리디/1이 될 때까지3.py
@@ -1,22 +1,22 @@
+# n이 100억 이상의 큰 수일 경우, n이 k의 배수가 되도록 한 번에 빼는 방식으로 작성
 n, k = map(int, input().split())
-ret = 0
+res = 0
 
-# n이 100억 이상의 큰 수일 경우
-#       n이 k의 배수가 되도록 한 번에 빼는 방식으로 작성
 while True:
-    # n이 k로 나눠지는 수가 될 때까지 1씩 빼기
+    # n이 k의 배수가 될 때까지 1씩 빼기
     target = (n // k) * k  # n이 k로 나눠 떨어지는 수 중에서 가장 큰 수
-    ret += (n - target)  # n에서 target을 빼면 n이 k로 나눠지는 수가 될 때까지 1씩 빼는 것과 같음
+    res += (n - target)  # n에서 target을 빼면 n이 k로 나눠지는 수가 될 때까지 1씩 빼는 것과 같음
     n = target  # n이 k로 나눠지는 수가 됨
 
-    # n이 k보다 작으면 나눌 수 없으므로 탈출
+    # n이 k보다 작으면 나눌 수 없음
     if n < k:
         break
 
+    # n을 k로 나누기
     n //= k
-    ret += 1
+    res += 1
 
 # 마지막으로 남은 수에 대해 1씩 빼기
 #       n이 3이라면 2(=3-1)번 빼야 1이 됨
-ret += (n - 1)
-print(ret)
+res += (n - 1)
+print(res)
