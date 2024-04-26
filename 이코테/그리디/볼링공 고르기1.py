31311529df52341e638af1f0913b641a6aadc6da
cabbage556 <liberty556@gmail.com>
2024-04-26 16:31:58 +0900

    그리디 문제 복습: 모험가 길드, 곱하기 혹은 더하기, 문자열 뒤집기, 만들 수 없는 금액, 볼링공 고르기

--- 이코테/그리디/볼링공 고르기1.py
+++ 이코테/그리디/볼링공 고르기1.py
@@ -1,20 +1,21 @@
-# 두 사람이 서로 무게가 다른 볼링공을 고르는 경우의 수 구하기
+"""
+볼링공 고르기
+    두 사람이 서로 무게가 다른 볼링공을 고르는 경우의 수를 구하기
+"""
 
-# 볼링공 개수, 공 최대 무게 입력
+# n : 볼링공 개수
+# m : 공 최대 무게
+# balls : 각 볼링공의 무게
 n, m = map(int, input().split())
-
-# 각 볼링공의 무게 입력
 balls = list(map(int, input().split()))
 
-# 경우의 수
+# res : 경우의 수
 res = 0
 
-# 사람 A
 for i in range(n):
-    # 사람 B
     for j in range(i + 1, n):
-        a = balls[i]
-        b = balls[j]
+        a = balls[i]  # 사람 A가 선택한 볼링공의 무게
+        b = balls[j]  # 사람 B가 선택한 볼링공의 무게
 
         # 두 사람이 선택한 공의 무게가 다르면 경우의 수 증가
         if a != b:
